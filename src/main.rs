553b13d837583aeb493a9e92e335b81e800d067b
sla-ppy <sla-ppy-pm@protonmail.com>
2025-02-14 20:28:38 +0100

    implement struct based obj

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,16 @@ use macroquad::prelude::*;
 use std::io;
 use std::io::*;
 
-struct Part {
+struct Component {
     voltage: f64,
     current: f64,
     resistance: f64,
-
     // rectangle params
-    posx: f32,
-    posy: f32,
-    width: f32,
-    height: f32,
+    x: f32,
+    y: f32,
+    w: f32,
+    h: f32,
 }
-
-fn create_component() {}
 
 fn draw_menu() {
     draw_text("Create component", screen_width() / 1.45, 32.0, 32.0, BLACK);
@@ -23,14 +20,15 @@ fn draw_menu() {
     draw_text("Move component", screen_width() / 1.45, 96.0, 32.0, BLACK);
 }
 
+impl Component {
+    fn new(voltage: f64, current: f64, resistance: f64, x: f32, y: f32, w: f32, h: f32) -> Component {
+        Component { voltage, current, resistance, x, y, w, h }
+    }
+}
+
 #[macroquad::main("Resistance Calculator")]
 async fn main() {
-    //create_component();
-
-    let posx = screen_width() / 10.0 - 60.0;
-    let posy = 100.0;
-    let width = 60.0;
-    let height = 30.0;
+    let component1 = Component::new(15.0, 2.0, 2.0, screen_width() / 10.0 - 60.0, 100.0, 60.0, 30.0);
     let offset = 100.0; // new part offset
 
     loop {
@@ -38,13 +36,12 @@ async fn main() {
 
         draw_menu();
 
-        draw_rectangle(posx, posy, width, height, BLACK);
-        draw_text("R1", posx + (width / 3.0), posy + (height / 1.5), 20.0, WHITE);
+        draw_rectangle(component1.x, component1.y, component1.w, component1.h, BLACK);
+        draw_text("R1", component1.x + (component1.w / 3.0), component1.y + (component1.h / 1.5), 20.0, WHITE);
 
-
-        draw_rectangle(posx + offset, posy, width, height, BLACK);
-        draw_line(posx + width, posy + (height / 2.0), posx + offset , posy + (height / 2.0), 2.5 ,BLACK);
-        draw_text("R2", posx + offset + (width / 3.0), posy + (height / 1.5), 20.0, WHITE);
+        //draw_rectangle(posx + offset, posy, width, height, BLACK);
+        //draw_line(posx + width, posy + (height / 2.0), posx + offset , posy + (height / 2.0), 2.5 ,BLACK);
+        //draw_text("R2", posx + offset + (width / 3.0), posy + (height / 1.5), 20.0, WHITE);
 
         if is_key_down(KeyCode::Escape) {
             return
